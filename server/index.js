7756d10817ba1f73342632c9eb76a760b0aaa20a
Mukul Rathore <59772989+mukul-rathore@users.noreply.github.com>
2021-09-07 17:21:02 +0530

    Update index.js
    complete

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://MukulRathore:MukulRathore123@cluster0.pzysa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = '';                                                                 // Mongo URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
